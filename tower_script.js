ae19b626fa96269a5acc5cb3e49c8dadfc467263
Mrkubsonallo <jakubek.c25@gmail.com>
2022-11-29 08:59:49 +0100

    gotowe branch main

--- tower_script.js
+++ tower_script.js
@@ -1,4 +1,4 @@
-window.onload = ring_check(), reset_game()
+window.onload = reset_game()
 
 function allowDrop(ev) 
 {
@@ -8,7 +8,6 @@ function allowDrop(ev)
 function drag(ev) 
 {
     ev.dataTransfer.setData("Text", ev.target.id)
-    //console.log(ev.dataTransfer.setData("Text", ev.target.id))
 }
   
 function drop(ev) 
@@ -16,16 +15,10 @@ function drop(ev)
     const stick1 = document.querySelector('#stick1')
     let drag_id, elem_id
     var data = ev.dataTransfer.getData("Text")
-    console.log(data[4])
 
     if(ev.target.children.length > 1)
     {
-        let fc = ev.target.children[0]
-        let par = ev.target.children[1]
-        let id_del = fc.getAttribute('id')
-        let elem = document.getElementById(id_del)
-        console.log(par.getAttribute('id')[4])
-        if(fc.getAttribute('id')[4] < par.getAttribute('id')[4])
+        if(data[4] < ev.target.children[0].getAttribute('id')[4])
         {
             ev.target.insertBefore(document.getElementById(data), ev.target.firstChild)
             ev.preventDefault()
@@ -44,32 +37,9 @@ function drop(ev)
         ev.preventDefault()
     }
 
-    //ring_check()
     end_game()
     draggable_attrib()
     
-}
-
-
-function ring_check()
-{
-    console.clear()
-    const stick1 = document.querySelector('#stick1')
-    const stick2 = document.querySelector('#stick2')
-    const stick3 = document.querySelector('#stick3')
-    console.log("stick1")
-    for(i=0; i<=stick1.children.length-1; i++)
-        console.log(stick1.children[i].getAttribute('id'))
-
-    console.log("")
-    console.log("stick2")
-    for(i=0; i<=stick2.children.length-1; i++)
-        console.log(stick2.children[i].getAttribute('id'))
-    
-    console.log("")
-    console.log("stick3")
-    for(i=0; i<=stick3.children.length-1; i++)
-        console.log(stick3.children[i].getAttribute('id'))  
 }
 
 function end_game()
@@ -138,5 +108,3 @@ function draggable_attrib()
     }
     
 }
-
-console.log(document.querySelector('#stick1').childNodes)
